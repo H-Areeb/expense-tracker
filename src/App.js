0393489dcc8a27d05b5c4285e9462ddadbfcb995
H-Areeb <areeburrehman13@gmail.com>
2021-11-23 21:08:24 +0500

    update app.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      Expense Tracker By Areeb Ur Rehman
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+            Expense Tracker app by Areeb ur Rehman.
+        </p>
+      </header>
     </div>
   );
 }
